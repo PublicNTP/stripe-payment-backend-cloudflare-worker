96f82fa6a328be42e8eeee451b3d6ef43a33ddec
Terry Ott <Terry.Ott@publicntp.org>
2020-12-16 23:33:25 +0000

    We're now getting payload body

--- pntp-payment-backend-cloudflare/src/handler.ts
+++ pntp-payment-backend-cloudflare/src/handler.ts
@@ -1,3 +1,17 @@
 export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
+
+    let requestResponse:Response;
+
+    // Make sure we got some JSON goodness in the payload
+    try {
+        const parsedJson = JSON.parse( await request.text() );
+
+        requestResponse = new Response( "Got valid JSON:\n" + JSON.stringify(parsedJson) );
+    } catch(e) {
+        const badParseResponse:{ [key:string] : string } = { error: 'Body contents did not successfully parse as JSON' };
+
+        requestResponse = new Response( JSON.stringify(badParseResponse) );
+    }
+        
+    return requestResponse;
 }
